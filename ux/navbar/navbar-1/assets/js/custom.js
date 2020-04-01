cf804264ee8f9af7a4da1e6f1aca0fab08607a4a
amar-nath-das <amarnath.das@tier5.in>
2020-04-01 09:17:44 +0530

    Navbar theme 1 v1.0.0

---
+++ ux/navbar/navbar-1/assets/js/custom.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var documentBody = document.querySelector("body");
+var menuTrigger = document.querySelector(".navbar .navbar-trigger");
+var mobileMenuTrigger = document.querySelector(".navbar .btnMobileTrigger");
+
+function triggerMenu() {
+  documentBody.classList.toggle("menu-active");
+}
+if (menuTrigger) {
+  menuTrigger.addEventListener("click", () => {
+    triggerMenu();
+  });
+}
+
+function mobileTrigger() {
+  documentBody.classList.toggle("mobileMenu-active");
+}
+
+if (mobileMenuTrigger) {
+  mobileMenuTrigger.addEventListener("click", () => {
+    mobileTrigger();
+  });
+}
