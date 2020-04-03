a73bb9def8a2daa73366ed1c833f250219c6698d
krishnokoli <ankita.sarkar@tier5.in>
2020-04-03 20:59:56 +0530

    all form responsive v0.2

---
+++ ux/modal/modal1/js/custom.js
@@ -0,0 +1,7 @@
+// Quick & dirty toggle to demonstrate modal toggle behavior
+
+document.getElementsByClassName("modal-toggle").onclick = function () {
+    alert(jsdjhkjshdkjfhkjshfkjhskj);   
+    e.preventDefault();
+    $('.modal').addClass('is-visible');
+};
